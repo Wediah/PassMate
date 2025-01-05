4079676fa05217035e3297096f4b5e7957ae0200
Wediah <100675006+Wediah@users.noreply.github.com>
2025-01-05 23:57:36 +0000

    adds validation for tickets

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tickets;
 
 class TicketsController extends Controller
 {
-    public function storeTicket(Request $request)
+    public function storeTicket(Request $request): void
     {
         $validated = $request->validate([
             'name' => 'required|max:255|min:2',
@@ -16,5 +17,21 @@ class TicketsController extends Controller
             'status' => 'required|numeric',
             'qr_code' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
+
+        if ($request->hasFile('qr_code')) {
+            $uploadedFileUrl = cloudinary()->upload($request->file('qr_code')->getRealPath())->getSecurePath();
+            $validated['qr_code'] = $uploadedFileUrl;
+        }
+
+        $ticket = [
+            'name' => $validated['name'],
+            'price' => $validated['price'],
+            'ticket_type' => $validated['ticket_type'],
+            'event_id' => $validated['event_id'],
+            'status' => $validated['status'],
+            'qr_code' => $validated['qr_code'],
+        ];
+
+        Tickets::create($ticket);
     }
 }
